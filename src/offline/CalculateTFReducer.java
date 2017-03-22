b776b4464e5e79eb81d9be2f26d780738c5a516f
Greg Poisson <gpoisson@rams.colostate.edu>
2017-03-22 07:34:00 -0600

    tf test

--- src/offline/CalculateTFReducer.java
+++ src/offline/CalculateTFReducer.java
@@ -28,22 +28,19 @@ public class CalculateTFReducer extends Reducer<Text,Text,Text,FloatWritable> {
 		
 		context.write(new Text(most_common), new FloatWritable());
 
-		ArrayList<Float> tfs = new ArrayList<Float>();
-		ArrayList<Text> words = new ArrayList<Text>();
+		//ArrayList<Float> tfs = new ArrayList<Float>();
+		//ArrayList<Text> words = new ArrayList<Text>();
 		
 		for (Text val: values) {
 			String[] entry = val.toString().split("\t");
 			String term = entry[0];
 			int count = Integer.parseInt(entry[1]);
 			float tf = (float) (0.5 + 0.5 * (((float) count) / max_occurrances));
-			tfs.add(tf);
-			words.add(new Text(key.toString() + " " + term));
+			context.write(new Text(key.toString() + " " + term), new FloatWritable (tf));
 		}
-		
-		context.write(new Text(tfs.size() + ""), new FloatWritable());
-		
-		for (int i = 0; i < tfs.size(); i++) {
-			context.write((words.get(i)), new FloatWritable(tfs.get(i)));
-		}
+				
+		//for (int i = 0; i < tfs.size(); i++) {
+		//	context.write((words.get(i)), new FloatWritable(tfs.get(i)));
+		//}
 	}
 }
