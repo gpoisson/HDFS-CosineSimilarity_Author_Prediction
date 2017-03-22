39d2f4118106b683d17fc2624ea8005c4eb40919
Greg Poisson <gpoisson@rams.colostate.edu>
2017-03-22 09:20:42 -0600

    testing idf

--- src/offline/CalculateIDFReducer.java
+++ src/offline/CalculateIDFReducer.java
@@ -2,20 +2,18 @@ package offline;
 
 import java.io.IOException;
 
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Reducer.Context;
 
-public class CalculateIDFReducer extends Reducer<Text,Text,Text,IntWritable> {
+public class CalculateIDFReducer extends Reducer<Text,Text,Text,FloatWritable> {
 
 	public void reduce(Text  key,  Iterable<Text>  values,  Context  context) throws IOException, InterruptedException {
-		int count = 0;
 		
 		for (Text val: values) {
-			count++;
+			context.write(key, new FloatWritable(Float.parseFloat(val.toString())));
 		}
-		
-		context.write(key, new IntWritable(count));
 	}
 }
